99feaef9e98993bb05c373f7b9a2d61d4089bf92
Damian Olszewski <damianolszewski25@gmail.com>
2023-01-11 18:16:20 +0100

    update

--- src/components/blog/blog-item/BlogItem.jsx
+++ src/components/blog/blog-item/BlogItem.jsx
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 import BlogAuthor from "../blog-author/BlogAuthor";
 import "./styles.css";
 const BlogItem = (props) => {
-  const { title, cover, author, _id } = props;
+  const { title, cover, author, _id, img, price, category, asin } = props;
   return (
-    <Link to={`/blog/${_id}`} className="blog-link">
+    <Link to={`books/${asin}`} className="blog-link">
       <Card className="blog-card">
-        <Card.Img variant="top" src={cover} className="blog-cover" />
+        <Card.Img variant="top" src={img} className="blog-cover" />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
         </Card.Body>
-        <Card.Footer>
-          <BlogAuthor {...author} />
+        <Card.Footer className="d-flex justify-content-between">
+          <p>{price}$</p>
+          <p>{category}</p>
         </Card.Footer>
       </Card>
     </Link>
--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -4,9 +4,26 @@ import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [news, setNews] = useState([]);
+
+  React.useEffect(() => {
+    const fetching = async () => {
+      let response = await fetch("damiansapi-main-production.up.railway.app");
+      if (response.ok) {
+        const fetchedData = await response.json();
+        setNews(fetchedData);
+      } else {
+        console.log("error");
+      }
+    };
+    fetching();
+  }, []);
+
+  console.log(news);
+  const blogstoprint = news.booksArray;
   return (
     <Row>
-      {posts.map((post) => (
+      {blogstoprint.map((post) => (
         <Col
           md={4}
           style={{
